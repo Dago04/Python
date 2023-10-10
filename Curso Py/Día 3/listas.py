22bbd86b547a592b466c7e225673dbe7093def69
Dago Salas <128066770+Dago04@users.noreply.github.com>
2023-10-10 14:05:07 -0600

    f

---
+++ Curso Py/Día 3/listas.py
@@ -0,0 +1,5 @@
+mi_lista = ['a','b','c']
+lista = ["Hola", 55, 6.1]
+
+print(lista)
+print(type(mi_lista))
---
+++ Curso Py/Día 3/propiedades string.py
@@ -0,0 +1,23 @@
+nombre = "carina"
+nombre ="Karina"
+
+print(nombre)
+
+n1= "Kari"
+n2= "na"
+
+print(n1+n2)
+print(n1 * 5)
+
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua"""
+
+print (poema)
+
+print("agua" in poema)
+
+print("agua" not in poema)
+
+print(len(poema))
+
